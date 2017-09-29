7c1852921bb3e9463e16a90ec3c372f83e3eff62
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-09-29 08:22:14 -0400

    updated to seperate js pages hotwire api WIP

--- assets/site.js
+++ assets/site.js
@@ -18,8 +18,8 @@ var config = {
 firebase.initializeApp(config);
 
 var gDatabase = firebase.database();
-var userCity = 'Orlando';
-var countryCode = '';
+
+
 
 /*
 Generates UUID based on the current Date / performance clock
@@ -85,16 +85,4 @@ var gUserCookie = setUserCookie();
 
 
 
-//runs function to grab weather for user selected city
-
-$(document).on('click', '#cityImg', cityWeather);
-
- userCity = $('#').val().trim();
-
-function cityWeather () {
-    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + userCity + '&APPID=c29d220ad91825204d613bb66ea4cbdf&cnt=16';
-    var weatherQuery = $.ajax({url: queryURL, method: 'Get'})
-    weatherQuery.done();
-    console.log('???'+ weatherQuery);
-};
 
