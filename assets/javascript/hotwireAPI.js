ddf5d4046c8a31cd88c1f47ea151b666c4446687
Sean Collins <sean_collins@gmx.com>
2017-09-29 21:24:30 -0400

    more cleanup, forgot to swap branch

--- assets/javascript/hotwireAPI.js
+++ assets/javascript/hotwireAPI.js
@@ -8,7 +8,11 @@ t = temporary (example: tUUID)
 function hotwireAPI (pStart, pEnd, pRegion) {
     var apiKey = 'krhcyf9u4tptfayz7zq26r4k';
 
-    var hotwireURL = `http://api.hotwire.com/v1/tripstarter/hotel?apikey=${apiKey}&startdate=${pStart}&enddate=${pEnd}&travelseason=low&format=JSON&limit=10`;
+    var hotwireURL = 'http://api.hotwire.com/v1/tripstarter/hotel'
+        +'?apikey='+ apiKey
+        +'&startdate='+ pStart
+        +'&enddate='+ pEnd
+        +'&travelseason=low&format=JSON&limit=10';
 
     var callHotwire = $.ajax({url: hotwireURL, method: 'GET'});
     callHotwire.done(init);
