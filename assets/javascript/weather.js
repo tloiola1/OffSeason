9d791b2f7fb0b66a27d6c45af53843febe3cb29d
Tarciso <tarcisoloiola@hotmail.com>
2017-10-02 00:33:21 -0400

    commit10/02

--- assets/javascript/weather.js
+++ assets/javascript/weather.js
@@ -1,22 +1,16 @@
-// var userCity = 'Orlando';
-// var countryCode = '';
+var userCity = 'Orlando';
+var countryCode = '';
 
 //runs function to grab weather for user selected city
-// $(document).on('click', '#cityImg', cityWeather);
+$(document).on('click', '#cityImg', cityWeather);
 //query hotwire API call
 
 
-// userCity = $('#').val().trim();
-// var newChar = $('#user-character').val();
-
-function cityWeather (location) {
-	console.log("This is "+location);
-    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + location + '&APPID=c29d220ad91825204d613bb66ea4cbdf&type=hour&start=1369728000&end=1369789200&cnt=6';
-    $.ajax({
-    	url: queryURL, 
-    	method: 'Get'
-    }).done(function(response){;
-
-    console.log(response);
-});
-}
+userCity = $('#').val().trim();
+var newChar = $('#user-character').val();
+function cityWeather () {
+    var queryURL = 'http://api.openweathermap.org/data/2.5/forecast?q=' + userCity + '&APPID=c29d220ad91825204d613bb66ea4cbdf&cnt=16';
+    var weatherQuery = $.ajax({url: queryURL, method: 'Get'})
+    weatherQuery.done();
+    console.log('???'+ weatherQuery);
+};
