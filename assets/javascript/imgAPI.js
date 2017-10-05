39231ea3d5de330675aeb243d536a9c6dba21ba0
scollins33 <sean_collins@gmx.com>
2017-10-05 00:48:51 -0400

    reducing files, promises up to CallImageAPI done

--- assets/javascript/imgAPI.js
+++ assets/javascript/imgAPI.js
@@ -1,32 +1,51 @@
 
-function imgAPI(pCity){
+function callImageAPI (response) {
+    console.log('starting callImageAPI');
+    console.log('using below response:');
+    console.log(response);
 
-  var key = "6588765-767f2672757f4bf26b7229992";
+    // define API Key and empty promise array
+    var imgApiKey = "6588765-767f2672757f4bf26b7229992";
+    var imageStack = [];
 
-  var queryURL = "https://pixabay.com/api/?key="+key+"&q="+ pCity +"&image_type=photo";
-// var queryURL = "https://maps.googleapis.com/maps/api/place/photo?&key=AIzaSyAQzWTq3aMdAEHpUdJhk_e0cRqDJaTbO1w";
+    // loop through passed response
+    for (var l = 0; l < response.length; l++) {
+        // get city name from current response object
+        var tCity = response[l]['DestinationCity'];
+        tCity = removeBad(tCity);
 
-    return $.ajax({
-      //URL Is Sending The Request w
-      url: queryURL,
-      //Method Is Getting The Response
-      method: "GET",
+        // build the query URL for the API
+        var queryURL = "https://pixabay.com/api/?key="+ imgApiKey
+            +"&q="+ tCity
+            +"&image_type=photo";
+
+        // add the AJAX call to the call stack
+        imageStack.push(
+            $.ajax({url: queryURL, method: "GET"})
+        );
+    }
+
+    // create a Promise All that waits for all of the calls to get responses
+    Promise.all(imageStack)
+        .then(function() {
+            // once they're all done pulling, loop through the responses
+            // assign to the original objects as imageURL
+            console.log('promises all done, imageStack below:');
+            console.log(imageStack);
+            for (var k = 0; k < imageStack.length; k++) {
+                var target = imageStack[k].responseJSON;
+                console.log('target below:');
+                console.log(target);
+                console.log(target.hits[0].webformatURL);
+                response[k]['imageURL'] = target.hits[0].webformatURL;
+                console.log(response[k])
+            }
+            console.log('the promises loop is done, new complete response below');
+            console.log(response);
     });
-      //.done Executes When Get The Response From URL
-    // }).done(function(response){
+}
 
-    //   console.log(response);
-
-    //   imgURL = response.hits[0].webformatURL;
-
-    //   return imgURL;
-    //   // var img = $("<img>");
-    //   // img.addClass("activator cityImg");
-    //   // img.attr("value", pCity);
-
-      // img.attr("src", response.hits[0].webformatURL);
-      // $(".card-image").html(img);
-      // $(".card-title").html(pCity);
-
-    // });
+// function to clean the input string
+function removeBad (pString) {
+    return pString.replace(/ *\([^)]*\) */g, "");
 }
--- assets/javascript/site.js
+++ assets/javascript/site.js
@@ -13,24 +13,28 @@ t = temporary (example: tUUID)
 // var gUserCookie = setUserCookie();
 // var gUUID = gUserCookie.uuid;
 
-// hide the cards results div
-// $("#card-wrapper").hide();
-
 // on-click function for the Domestic button
 $('#domestic').on('click', function (event) {
     event.preventDefault();
 
     // pull the values
-    var startDate = $('#start-date').val();
-    var endDate = $('#end-date').val();
+    var userStart = $('#start-date').val();
+    var userEnd = $('#end-date').val();
     var regionPicked = 'domestic';
+
+    // convert the dates using convertDateHotwire from hotwireAPI.js
+    var convertedDates = convertDateHotwire(userStart, userEnd);
+    var startDate = convertedDates[0];
+    var endDate = convertedDates[1];
 
     // clear out old results
     $('#result-cards').empty();
 
     // summon hotwireAPI from hotwireAPI.js
     // pass the start, end, and region
-    hotwireAPI(startDate, endDate, regionPicked);
+    callHotwireAPI(startDate, endDate)
+        .then(response => parseHotwire(response, regionPicked))
+        .then(response => callImageAPI(response));
 });
 
 // on-click function for the International button
@@ -38,18 +42,26 @@ $('#international').on('click', function (event) {
     event.preventDefault();
 
     // pull the values
-    var startDate = $('#start-date').val();
-    var endDate = $('#end-date').val();
+    var userStart = $('#start-date').val();
+    var userEnd = $('#end-date').val();
     var regionPicked = 'international';
+
+    // convert the dates using convertDateHotwire from hotwireAPI.js
+    var convertedDates = convertDateHotwire(userStart, userEnd);
+    var startDate = convertedDates[0];
+    var endDate = convertedDates[1];
 
     // clear out old results
     $('#result-cards').empty();
 
     // summon hotwireAPI from hotwireAPI.js
     // pass the start, end, and region
-    hotwireAPI(startDate, endDate, regionPicked);
+    callHotwireAPI(startDate, endDate)
+        .then(response => parseHotwire(response, regionPicked))
+        .then(response => callImageAPI(response));
 });
 
+// handle the calendars on the landing page
 $('.datepicker').pickadate({
     selectMonths: true,
     selectYears: 15,
